6ce4761cad15ec6040c2907a9a80f7479abb473f
jd2rogers2 <jd2rogers2@gmail.com>
2019-07-20 19:24:29 -0400

    tests connected to solution file

---
+++ solution.js
@@ -0,0 +1,13 @@
+// given
+// const points = {
+//   a: {b: 6, d: 1},
+//   b: {a: 6, c: 5, d: 2, e: 2},
+//   c: {b: 5, e: 5},
+//   d: {a: 1, b: 2, e: 1},
+//   e: {d: 1, b: 2, c: 5}
+// }
+// find shortest path from a to e
+
+module.exports = {
+  solution: points => {}
+};
---
+++ test/test.js
@@ -0,0 +1,9 @@
+require('mocha');
+var expect = require('chai').expect;
+const { solution } = require('../solution');
+
+describe('test', () => {
+  it('test 1', () => {
+    expect(solution()).to.equal(undefined);
+  });
+});
