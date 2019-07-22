c6ef9c4933b4ef9f89f74d5ec82342595fd27170
jd2rogers2 <jd2rogers2@gmail.com>
2019-07-22 14:50:21 -0400

    built get all routes and passing tests

--- solution.js
+++ solution.js
@@ -8,6 +8,57 @@
 // }
 // find shortest path from a to e
 
+// recursively find all paths to end
+// get each path's total
+
+// get all possible routes
+// get time for each route
+// get fastest route
+
+const getAllRoutes = (points, start, end) => {
+  let dictionary = {};
+  recursion(points, [start], end, dictionary);
+  return Object.values(dictionary);
+}
+
+const recursion = (points, currentPath, end, pathsDictionary) => {
+  if (currentPath[currentPath.length - 1] === end) {
+    pathsDictionary[Object.keys(pathsDictionary).length] = currentPath;
+    return;
+  }
+
+  let nextNode = currentPath[currentPath.length - 1];
+  let children = Object.keys(points[nextNode]);
+  for (let x = 0; x < children.length; x++) {
+    if (!currentPath.includes(children[x])) {
+      recursion(points, [...currentPath, children[x]], end, pathsDictionary);
+    }
+  }
+}
+
+const getSortedRouteTimes = (points, start, end) => {
+  let routes = getAllRoutes(points, start, end);
+  let routeTimes = [];
+  for (let y = 0; y < routes.length; y++) {
+    routeTimes.push([routes[y], getRoutesTime(points, routes[y])]);
+  }
+  return routeTimes.sort((a, b) => a[1] - b[1]);
+}
+
+const getRoutesTime = (points, route) => {
+  let time = 0;
+
+  for (let z = 0; z < route.length - 1; z++) {
+    time += points[route[z]][route[z + 1]];
+  }
+
+  return time;
+}
+
+const getFastestRoute = (points, start, end) => getSortedRouteTimes(points, start, end)[0];
+
 module.exports = {
-  solution: points => {}
+  getAllRoutes,
+  getSortedRouteTimes,
+  getFastestRoute
 };
--- test/test.js
+++ test/test.js
@@ -1,9 +1,35 @@
 require('mocha');
 var expect = require('chai').expect;
-const { solution } = require('../solution');
+const { getFastestRoute } = require('../solution');
 
-describe('test', () => {
-  it('test 1', () => {
-    expect(solution()).to.equal(undefined);
+const points = {
+  a: {b: 6, d: 1},
+  b: {a: 6, c: 5, d: 2, e: 2},
+  c: {b: 5, e: 5},
+  d: {a: 1, b: 2, e: 1},
+  e: {d: 1, b: 2, c: 5}
+}
+
+//
+//
+// !!!!! these should return the paths instead
+//
+//
+
+describe('getFastestRoute', () => {
+  it('from a to c is 7', () => {
+    expect(getFastestRoute(points, 'a', 'c')[1]).to.equal(7);
+  });
+
+  it('from a to e is 2', () => {
+    expect(getFastestRoute(points, 'a', 'e')[1]).to.equal(2);
+  });
+
+  it('from d to c is 6', () => {
+    expect(getFastestRoute(points, 'd', 'c')[1]).to.equal(6);
+  });
+
+  it('from a to b is 7', () => {
+    expect(getFastestRoute(points, 'a', 'b')[1]).to.equal(3);
   });
 });
